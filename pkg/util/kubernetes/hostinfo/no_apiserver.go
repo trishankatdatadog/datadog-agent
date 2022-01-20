194feb17a740c7c189699c395f667ab333f6f54b
Kylian Serrania <kylian.serrania@datadoghq.com>
2022-01-20 17:58:17 +0100

    Fix trace-agent OTLP config tests

--- pkg/util/kubernetes/hostinfo/no_apiserver.go
+++ pkg/util/kubernetes/hostinfo/no_apiserver.go
@@ -3,8 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build kubelet
-//go:build !kubeapiserver
+//go:build kubelet && !kubeapiserver
 
 package hostinfo
 
